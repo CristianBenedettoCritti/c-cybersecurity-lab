eaea11d7dadc1f30ae2bf16882eeaad10379f693
TAACRCR1 <Cristian.critti@swisscom.com>
2026-04-16 15:52:55 +0200

    feat: :sparkles: File Reader that checks for keywords in files
    This is a File reader that checks every line in the file for specific keywords and then will tell u in which line they found it.

---
+++ 2_The File Scanner/main.c
@@ -0,0 +1,49 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 101
+
+int analyzeFile(char *fileName);
+int main(void){
+    char *fileName = malloc(SIZE);
+
+    if(!fileName) {
+        perror("malloc failed!");
+        return EXIT_FAILURE;
+    }
+
+    printf("Enter File Name: ");
+
+    if(fgets(fileName, SIZE, stdin) == NULL) {
+        perror("fgets failed!");
+        free(fileName);
+        return EXIT_FAILURE;
+    }
+    fileName[strcspn(fileName, "\n")] = '\0';
+
+    analyzeFile(fileName);
+    free(fileName);
+}
+
+int analyzeFile(char *fileName) {
+    char line[256];
+    char *keywords[] = {"API_KEY", "password", "token"};
+    int lineCounter = 1;
+    FILE *ftpr = fopen(fileName, "r");
+    if(ftpr == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
+    
+    while (fgets(line, sizeof(line), ftpr)) {
+        // fgets(buffer, size, file_pointer)
+        for (int i = 0; i < sizeof(keywords) / sizeof(keywords[0]); i++){
+            if(strstr(line, keywords[i]) != NULL) {
+                printf("\nFound secret on line %d: %s", lineCounter, line);
+            }
+        }
+        lineCounter++;
+    }
+
+    fclose(ftpr);
+}
